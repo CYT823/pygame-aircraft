3bd9505ac98e152f64ed801885ed6b7f656c47df
CYT823 <40158342+CYT823@users.noreply.github.com>
2022-08-27 23:38:20 +0800

    update main
    add sprites (game objects)
     -player
     -rock

--- main.py
+++ main.py
@@ -4,13 +4,67 @@ References:
     [2] Pygame tutorial, https://youtu.be/61eX0bFAsYs
 '''
 import pygame
+import random
 
 # parameters
 running = True
+WIDTH = 500
+HEIGHT = 600
+FPS = 60
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# create player object
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(BLUE)
+        self.rect = self.image.get_rect() # create rectangle for the surface
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 8
+
+    def update(self):
+        key_pressed = pygame.key.get_pressed()
+        if key_pressed[pygame.K_RIGHT]:
+            self.rect.x += self.speedx
+        elif key_pressed[pygame.K_LEFT]:
+            self.rect.x -= self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        elif self.rect.left < 0:
+            self.rect.left = 0
+
+# create rock object
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((30, 40))
+        self.image.fill(RED)
+        self.rect = self.image.get_rect() # create rectangle for the surface
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedx = random.randrange(-3, 3)
+        self.speedy = random.randrange(2, 10)
+
+    def update(self):
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        
+        if self.rect.top > HEIGHT or self.rect.left > WIDTH or self.rect.right < 0:
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedx = random.randrange(-3, 3)
+            self.speedy = random.randrange(2, 10)
+
 
 # init game and build game window
 pygame.init()
-pygame.display.set_mode((500, 600))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("pygame-aircraft")
 
 # create joy decvices objects
 pygame.joystick.init()
@@ -19,9 +73,19 @@ joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_coun
 # clock object
 clock = pygame.time.Clock()
 
+# create sprites(game objects)
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+for i in range(8):
+    rock = Rock()
+    all_sprites.add(rock)
+
 # looping...
 while running:
-    clock.tick(60) # Program will never run at more than 40 frames per second.
+    # FPS setting
+    clock.tick(FPS) # Program won't run more than 60 frames per second.
+    
     # user input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -29,8 +93,16 @@ while running:
         # elif event.type == pygame.KEYDOWN:
         #     if event.key == "K_q":
         #         running = False
+    
     # upadte game
+    all_sprites.update() # call every game object's update function
 
     # render
-    
+    screen.fill(WHITE)
+    all_sprites.draw(screen)
+    pygame.display.update()
+
 pygame.quit()
+
+# pygame.sprite
+# pygame module with basic game object classes
